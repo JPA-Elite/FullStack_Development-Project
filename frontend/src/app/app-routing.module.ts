ebf4521f641e17f01d8b1305dcabeed0754d2846
JPA-EliteDeveloper <algadipej962@gmail.com>
2023-01-24 08:30:18 +0800

    save

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
     path : 'transactions', component: ArticlesComponent
   },
   {
-    path : 'home', component: HomeComponent
+    path : '', component: HomeComponent
   }
 ];
 
